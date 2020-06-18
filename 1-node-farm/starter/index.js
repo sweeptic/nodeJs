bbadd75a24089891d34bf9b9bfb2cb5cd9b3c95f
Attila <59293104+sweeptic@users.noreply.github.com>
2020-06-18 18:52:21 +0200

    remove olda data

--- 1-node-farm/starter/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-
-console.log(textIn);
-
-const textOut = `this is what we know about avokado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log('File written')
