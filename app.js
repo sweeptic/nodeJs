3463af7d34030daa404d8e7391fffc6b7605a256
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-05 18:48:40 +0200

    4. Adding Middleware

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const express = require('express');
 //express is a framework.
 const app = express();
 
+
+app.use((req, res, next) => {
+   console.log('in the middleware!');
+   next();
+});
+
+
+app.use((req, res, next) => {
+   console.log('in the middleware 2 !')
+   // ...
+});
+
 const server = http.createServer(app);
 
 server.listen(3010); 
