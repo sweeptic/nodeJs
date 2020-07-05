55c6c60c4f88bfafa29496a92cb2cb5b3d6b3a7c
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-05 18:54:14 +0200

    5. How Middleware Works

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
    console.log('in the middleware 2 !')
-   // ...
+   res.send('<h1>Hello from Express!</h1>');
 });
 
 const server = http.createServer(app);
