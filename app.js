4050e9b9358db98457d49fb5434cfad0ce64586e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-06 14:58:13 +0200

    9. Parsing Incoming Requests

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', (req, res, next) => {
-   console.log("this allways runs ! ");
    next();
 });
 
 
 app.use('/add-product', (req, res, next) => {
-   res.send('<h1>the add product page</h1>');
+   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+   console.log(req.body);
+   res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
