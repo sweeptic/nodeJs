514751996e46c21342344f07a7ded62af3605158
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-06 15:55:28 +0200

    13. Filtering Paths

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
    res.status(404).send('<h1>Page not found</h1>')
 });
 
