a9800cf1e6b9d483d454f3f060ea0f5110e119d5
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-09 19:14:40 +0200

    2. Sharing Data Across Requests & Users

--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+   console.log('shop.js', adminData.products);
    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
