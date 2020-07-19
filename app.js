1da2159099aa8e76dc42bbde1f325a34e97f7d25
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-19 19:00:59 +0200

    14. Working with EJS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
