57184dcd41257dca8ecf3ed41dd8ea976cb93b79
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-22 17:45:50 +0200

    3. Adding Controllers

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
