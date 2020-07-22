c078c42cb159c44080eaa43a4e106981dff1690f
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-22 17:15:14 +0200

    rearrange files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 
@@ -16,11 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-
 app.use((req, res, next) => {
-   res.status(404).render('404', {pageTitle: 'Page Not Found'});
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-
-
-app.listen(3010); 
+app.listen(3010);
--- routes/shop.js
+++ routes/shop.js
@@ -2,18 +2,21 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-   //delete before use pug templating engine
-   // console.log('shop.js', adminData.products);
-   // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
-
-   const products = adminData.products;
-   res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
-
 
 module.exports = router;
