5e17ec0d4c9b65edb34193dea154f47817bfafae
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-30 20:04:32 +0200

    15. Deleting Cart Items

--- controllers/admin.js
+++ controllers/admin.js
@@ -60,3 +60,10 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products')
+}
