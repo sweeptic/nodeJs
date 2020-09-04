50d0d63ae65036cd2fc5f1abef44c3a7facd1d6c
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-04 14:00:33 +0200

    set up mysql

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-
+const db = require('./util/database')
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+   .then(result => {
+      console.log(result[0])
+   })
+   .catch(err => {
+      console.log(err)
+   })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
