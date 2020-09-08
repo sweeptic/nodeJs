d2b5a4eed0ac46fc778adb0939cab0a95534958a
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-08 20:56:07 +0200

    17. Fetching Related Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -36,8 +36,12 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect('/');
       }
@@ -81,7 +85,8 @@ exports.postEditProduct = (req, res, next) => {
 
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user.getProducts()
     .then(
       products => {
         res.render('admin/products', {
