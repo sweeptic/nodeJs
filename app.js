ad08a683400829b3cf9478bab9efb545ed2a3260
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-08 18:04:34 +0200

    5. Syncing JS Definitions to the Database

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-const db = require('./util/database')
+const sequelize = require('./util/database')
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,4 +19,11 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
 
-app.listen(3010);
+sequelize.sync().then(result => {
+   app.listen(3010);
+   // console.log(result)
+})
+.catch(err => {
+   // console.log(err)
+})
+
--- models/product.js
+++ models/product.js
@@ -3,27 +3,31 @@ const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
 const Product = sequelize.define('product', {
+
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true
   },
-  title: {
+
+  title: Sequelize.STRING,
+
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+
+  imageUrl: {
     type: Sequelize.STRING,
-    price: {
-      type: Sequelize.DOUBLE,
-      allowNull: false,
-    },
-    imageUrl: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    }
+    allowNull: false,
+  },
+  
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
   }
+
 });
 
 module.exports = Product;
