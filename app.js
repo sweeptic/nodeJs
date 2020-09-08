a253b0e1351039b0279cae6d71aec8ec2067252e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-08 20:27:36 +0200

    15. Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+   User.findByPk(1)
+      .then(user => {
+         req.user = user;
+         next();
+      })
+      .catch(err => console.log(err))
+})
+
 //routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -28,10 +37,22 @@ User.hasMany(Product);
 
 
 sequelize
-   .sync({ force: true })
+   // .sync({ force: true })
+   .sync()
    .then(result => {
-      app.listen(3010);
+      return User.findByPk(1)
+      // app.listen(3010);
       // console.log(result)
+   })
+   .then(user => {
+      if (!user) {
+         return User.create({ name: 'Max', email: 'test@test.com' })
+      }
+      return user;
+   })
+   .then(user => {
+      // console.log(user);
+      app.listen(3010);
    })
    .catch(err => {
       // console.log(err)
