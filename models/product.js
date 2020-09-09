4fb9f95314ce81b564ee749e5d4f424c76904a09
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-09 14:13:34 +0200

    11. Fetching All Products

--- models/product.js
+++ models/product.js
@@ -17,6 +17,20 @@ class Product {
       })
       .catch(err => console.log(err));
   }
+
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => { console.log(products); return products; })
+      .catch(err => console.log(err));
+  }
+
+
+
 }
 
 
