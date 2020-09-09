1867d2d2d4fb085a189c5972575fc865c71caf0c
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-09 14:40:50 +0200

    12. Fetching a Single Product

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
-const getDb = require('../util/database').getDb
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+
 
 class Product {
   constructor(title, price, description, imageUrl) {
@@ -26,6 +28,16 @@ class Product {
       .find()
       .toArray()
       .then(products => { console.log(products); return products; })
+      .catch(err => console.log(err));
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => { console.log(product); return product; })
       .catch(err => console.log(err));
   }
 
