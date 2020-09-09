847fa97d8a62776899f06cd7a0e2ed7b118bd2e0
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-09 16:30:55 +0200

    21. Working on Cart Items & Orders

--- models/user.js
+++ models/user.js
@@ -4,9 +4,11 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.email = email;
     this.name = username
+    this.cart = cart
+    this._id = id;
   }
 
   save() {
@@ -14,9 +16,18 @@ class User {
     return db.collection('users').insertOne(this);
   }
 
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] }
+    const db = getDb();
+    return db.collection('users').updatedOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
+  }
+
   static findBy(user) {
     const db = getDb();
-    return db.collection('users').find({ _id: new ObjectId(userId)}).next();
+    return db.collection('users').find({ _id: new ObjectId(userId) }).next();
   }
 }
 
