3bd92b29331521bce1c70ee868b2b70d07e812b0
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-09 09:32:57 +0200

    18. One-To-Many & Many-To-Many Relations

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const sequelize = require('./util/database')
 
 const Product = require('./models/product')
 const User = require('./models/user')
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -34,11 +36,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
-
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-   // .sync({ force: true })
-   .sync()
+   .sync({ force: true })
+   // .sync()
    .then(result => {
       return User.findByPk(1)
       // app.listen(3010);
