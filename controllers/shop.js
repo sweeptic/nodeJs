7bf56ba091a9c9722ac690e1bca0c94985c9494b
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-10 17:54:49 +0200

    18. Clearing the Cart After Storing an Order

--- controllers/shop.js
+++ controllers/shop.js
@@ -87,7 +87,7 @@ exports.postOrder = (req, res, next) => {
     .execPopulate()
     .then(user => {
       const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: { ...i.productId._doc }}
+        return { quantity: i.quantity, product: { ...i.productId._doc } }
       });
       const order = new Order({
         user: {
@@ -99,6 +99,9 @@ exports.postOrder = (req, res, next) => {
       order.save()
     })
     .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
