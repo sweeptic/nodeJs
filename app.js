c6a1b56dd7b2df251950b96e497fa8151e069db8
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-11 18:30:44 +0200

    10. Initializing the Session Middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,6 +19,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: '!+%G!THghfdgre+%R43trgfd44', resave: false, saveUninitialized: false }))
 
 app.use((req, res, next) => {
   User.findById('5f5a305ba4f17f3cfcb5bc01')
