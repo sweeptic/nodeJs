3eb36da3c197de4539221a845e0cabeb229a797d
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-11 16:25:31 +0200

    4. Optional Creating the Login Form

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+// const path = require('path');
+const express = require('express');
+
+const router = express.Router();
+
+const authController = require('../controllers/auth');
+
+
+router.get('/login', authController.getLogin)
+
+
+
+module.exports = router;
