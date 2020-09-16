4b94f7b001c1fc3154859248b9af8dbca5e0cc6c
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-16 14:33:45 +0200

    7. More Validators

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -13,15 +13,23 @@ router.post('/login', authController.postLogin);
 
 router.post(
    '/signup',
-   check('email')
-      .isEmail()
-      .withMessage('Please enter valid email.')
-      .custom((value, { req }) => {
-         if (value === 'test@test.com') {
-            throw new Error('This email address is forbidden.')
-         }
-         return true;
-      }),
+   [
+      check('email')
+         .isEmail()
+         .withMessage('Please enter valid email.')
+         .custom((value, { req }) => {
+            if (value === 'test@test.com') {
+               throw new Error('This email address is forbidden.')
+            }
+            return true;
+         }),
+      body(
+         'password',
+         'Please enter a password with only numbers and text and least 5 characters.')
+         .isLength({ min: 5 })
+         .isAlphanumeric()
+   ],
+
    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
