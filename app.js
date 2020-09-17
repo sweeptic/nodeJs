dba147650831683fa1f07fbcd8580aab05433341
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-17 11:13:43 +0200

    5. Throwing Errors in Code

--- app.js
+++ app.js
@@ -47,10 +47,16 @@ app.use((req, res, next) => {
   }
   User.findById(req.session.user._id)
     .then(user => {
+      if (!user) {
+        next();
+      }
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      throw new Error(err)
+      // next();
+    });
 });
 
 app.use((req, res, next) => {
