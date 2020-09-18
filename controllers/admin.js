1a78c4ce9a424a66ef883a1d96f77ae942e7fa31
Attila <59293104+sweeptic@users.noreply.github.com>
2020-09-18 11:05:41 +0200

    10. Downloading Files with Authentication

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,6 +20,9 @@ exports.postAddProduct = (req, res, next) => {
   const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+
+  
+
   if (!image) {
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
@@ -35,6 +38,10 @@ exports.postAddProduct = (req, res, next) => {
       validationErrors: []
     });
   }
+
+
+
+
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,8 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
+const fs = require('fs');
+const path = require('path');
+
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -146,3 +149,46 @@ exports.getOrders = (req, res, next) => {
       return next(error);
     });
 };
+
+// exports.getInvoice = (req, res, next) => {
+//   const orderId = req.params.orderId;
+//   //   Order.findById(orderId)
+//   //     .then(order => {
+//   //       if (!order) {
+//   //         return next(new Error('No order found.'));
+//   //       }
+//   //       if (order.user.userId.toString() !== req.user._id.toString()) {
+//   //         return next(new Error('Unauthorized'));
+//   //       }
+//   const invoiceName = 'invoice-' + orderId + '.pdf';
+//   const invoicePath = path.join('data', 'invoices', invoiceName);
+//   fs.readFile(invoicePath, (err, data) => {
+
+//     if (err) {
+//       return next(err);
+//     } else {
+//       //         res.setHeader('Content-Type', 'application/pdf');
+//       //         res.setHeader(
+//       //           'Content-Disposition',
+//       //           'inline; filename="' + invoiceName + '"'
+//       //         );
+//       res.send(data);
+//     }
+//   });
+//   //     })
+//   //     .catch(err => next(err));
+// };
+
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName)
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err); //default error handling function take over. other code dont execute
+    } else {
+      res.send(data);
+    }
+  })
+}
